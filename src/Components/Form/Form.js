a1e2f2bbd229c72ebb25b3137cc5605a53db1a7f
MurrTuck <murrtuck@gmail.com>
2020-05-29 14:19:49 -0600

    Updating Form

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -5,16 +5,38 @@ class Form extends Component {
         super()
 
         this.state = {
-
+            imageUrl: '',
+            productName: '',
+            productPrice: 0.00
         }
 
+    }
+
+    updateState(prop, value) {
+        this.setState({
+            [prop]: value
+        })
     }
 
 
 
     render() {
+        // console.log(this.state)
         return (
-            <div>Form</div>
+            <div>
+                <div>Form</div>
+                <div>
+                    <input onChange={(e) => this.updateState('imageUrl', e.target.value)} placeholder="Image URL" />
+                </div>
+                <div>
+                    <input onChange={(e) => this.updateState('productName', e.target.value)} placeholder='Add Product Name' />
+                </div>
+                <div>
+                    <input onChange={(e) => this.updateState('productPrice', e.target.value)} placeholder='0.00' />
+                </div>
+                <button>Cancel</button>
+                <button>Add to Inventory</button>
+            </div>
         )
     }
 }
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -14,7 +14,7 @@ class Header extends Component {
 
     render() {
         return (
-            <div>Header</div>
+            <div>Shelfie</div>
         )
     }
 }
